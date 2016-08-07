00fbf8ed9fe4dc00b2493d891c5446854bae4369
Odarchenko N.D <odarchenko.n.d@gmail.com>
2016-08-07 13:52:23 +0300

    ads

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -51,6 +51,17 @@
             @endif
         </div>
     </article>
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:728px;height:90px"
+         data-ad-client="ca-pub-7438604017777834"
+         data-ad-slot="2050350497"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
+
     <div class="row" id="comment-form">
         <h4>Оставить комментарий</h4>
         @if (count($errors) > 0)
